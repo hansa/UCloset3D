01d49ecb9113b49a2baeb7030e0d3889b9808e33
hansa <hansa7@gmail.com>
2025-06-21 17:28:12 -0700

    Add outfit upload component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UploadPhotoComponent } from './components/upload-photo/upload-photo.component';
 import { AvatarViewComponent } from './components/avatar-view/avatar-view.component';
 import { MixMatchComponent } from './components/mix-match/mix-match.component';
+import { UploadOutfitsComponent } from './components/upload-outfits/upload-outfits.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'upload', pathMatch: 'full' },
   { path: 'upload', component: UploadPhotoComponent },
   { path: 'avatar', component: AvatarViewComponent },
+  { path: 'upload-outfits', component: UploadOutfitsComponent },
   { path: 'mix-match', component: MixMatchComponent },
   { path: '**', redirectTo: 'upload' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { AvatarViewComponent } from './components/avatar-view/avatar-view.component';
 import { UploadPhotoComponent } from './components/upload-photo/upload-photo.component';
 import { MixMatchComponent } from './components/mix-match/mix-match.component';
+import { UploadOutfitsComponent } from './components/upload-outfits/upload-outfits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AvatarViewComponent,
     UploadPhotoComponent,
-    MixMatchComponent
+    MixMatchComponent,
+    UploadOutfitsComponent
   ],
   imports: [
     BrowserModule,
